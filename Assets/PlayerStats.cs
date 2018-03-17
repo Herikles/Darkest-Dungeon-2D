7668657f57e0236dfa03443e3df20fb3fb6bd4a2
Herikles <37475248+Herikles@users.noreply.github.com>
2018-03-17 13:49:03 +0100

    Initial commit

---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerStats : MonoBehaviour {
+    new Random Würfel;
+
+    string DD = ("Damage Dealer");
+
+
+    int Armor = 0;
+
+	// Use this for initialization
+	void Start () {
+		if (tag == DD)
+        {
+            Dice(1, 3);
+            var MaxHealthDD = 7 + Armor + j;
+
+        }
+	}
+
+    void Dice(int Min, int Max)
+    {
+       public int j = Würfel.Range(Min, Max);
+    }
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+}
